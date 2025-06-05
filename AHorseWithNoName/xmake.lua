b6e4fee730711f3d255ba873c34dccb16bab9944
Xebeth <github@north-edge.com>
2025-06-05 04:35:11 +0100

    Add include files to the project

--- AHorseWithNoName/xmake.lua
+++ AHorseWithNoName/xmake.lua
@@ -4,6 +4,7 @@ target(projectName)
 	add_rules("ue4ss.mod")
 
 	add_includedirs("include", "include/Altar", "include/Engine")
+	add_headerfiles("include/**.h", "include/Altar/**.h", "include/Engine/**.h")
 	add_files("src/dllmain.cpp", "src/HorseHandler.cpp", "src/Altar/VPairedPawn.cpp", "src/Altar/VPairedCharacter.cpp",
 			  "src/Altar/VAltarPlayerController.cpp", "src/Altar/TESForm.cpp", "src/Altar/VTESObjectRefComponent.cpp",
 			  "src/Altar/UAltarCheatManager.cpp",  "src/Engine/PlayerController.cpp", "src/Engine/UCheatManager.cpp",
