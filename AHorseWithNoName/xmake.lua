cc6cac646fc29999a9af0d33a3fec9f30f1fbaca
Xebeth <github@north-edge.com>
2025-06-08 13:15:14 +0100

    Update xmake file with wildcards

--- AHorseWithNoName/xmake.lua
+++ AHorseWithNoName/xmake.lua
@@ -3,9 +3,7 @@ local projectName = "AHorseWithNoName"
 target(projectName)
 	add_rules("ue4ss.mod")
 
-	add_includedirs("include", "include/Altar", "include/Engine")
+	add_includedirs("include", "include/Altar", "include/Engine", "include/Config")
 	add_headerfiles("include/**.h", "include/Altar/**.h", "include/Engine/**.h")
-	add_files("src/dllmain.cpp", "src/HorseHandler.cpp", "src/Altar/VPairedPawn.cpp", "src/Altar/VPairedCharacter.cpp",
-			  "src/Altar/VAltarPlayerController.cpp", "src/Altar/TESForm.cpp", "src/Altar/VTESObjectRefComponent.cpp",
-			  "src/Altar/UAltarCheatManager.cpp",  "src/Engine/PlayerController.cpp", "src/Engine/UCheatManager.cpp",
-			  "src/Altar/TESActorBase.cpp")
+
+	add_files("src/**.cpp", "src/Altar/**.cpp", "src/Engine/**.cpp", "src/Config/**.cpp")
